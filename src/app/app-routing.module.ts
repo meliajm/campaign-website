b0f9eefc18379f20c627e3ca6eabbccf4172edf1
LizaMarie <lizamariejimenez@gmail.com>
2020-11-20 11:26:48 -0500

    added timeline

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BioComponent } from './components/bio/bio.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { IssueComponent } from './components/issue/issue.component';
 // import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 const routes: Routes = [
   // {
   //   path: '',
   //   component: NavBarComponent
-  // }
+  // },
+   {
+    path: 'bio',
+    component: BioComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'issue',
+    component: IssueComponent
+  }
 ];
 
 @NgModule({
